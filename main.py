d75397efa0a5ef2b6dc7d3440b7c34335d27797f
CarEsteban <car23016@uvg.edu.gt>
2023-06-01 01:08:58 -0600

    Created graphics
    Interfaz visual con turtle agregada a todos lados

--- main.py
+++ main.py
@@ -6,15 +6,58 @@
 
 import pandas as pd
 from modulos import *
- 
-ciclo = True
+import turtle
 
+#variables del proyecto
+ciclo = True
+mouse = turtle.Turtle()
+screen_width, screen_height = turtle.screensize()
+window = turtle.Screen()
+
+#Configuraciones de turlte
+turtle.setup(width=0.5, height=0.5, startx=None, starty=None)
+turtle.setworldcoordinates(0, 0, screen_width, screen_height)
+mouse.speed(0)
+
+def pantallaInicio():
+    #Configuraciones de la ventana principal
+    window.title("STUDIFY")
+    window.bgcolor("#DE9E36")
+
+
+
+    #Configuraciones del texto principal
+    texto_inicio = "Bienvenidos a Studify"
+    mouse.hideturtle()
+    font_size = 40
+    mouse.color("#37323E")
+
+    #Posicionamiento en el centro
+    pos_x = screen_width / 2
+    pos_y = screen_height / 1.5
+
+    mouse.penup()
+    mouse.goto(pos_x, pos_y)
+
+    font = ("Comic Sans MS", font_size, "bold")
+    mouse.write(texto_inicio, align="center", font=font)
+
+
+    # Creación de los subtítulos
+    subtitulos = ["1. Iniciar Sesión", "2. Registrarse", "3. Modificar Datos", "4. Mostrar Usuarios", "5. Salir"]
+    y_pos_sub = pos_y - 50  # Posición vertical inicial
+
+    for subtitulo in subtitulos:
+        mouse.goto(pos_x, y_pos_sub)
+        mouse.write(subtitulo, align="center", font=("Arial", 16))
+        y_pos_sub -= 30  # Desplazamiento vertical para el siguiente subtítulo
 
 
 while ciclo == True:
+    borrarVentana(window)
+    pantallaInicio()
+    
     df = pd.read_csv("Usuarios.csv")
-    print("Bienvenido a Studify")
-    print("1. Iniciar Sesion: \n2. Registrarse: \n3. Modificar Datos \n4. Mostrar usuarios \n5. Salir")
     
     try:
         opcion = int(input("Que desea hacer? "))
@@ -30,9 +73,14 @@ while ciclo == True:
     os.system("cls")
                
     if opcion == 1:
-        print("BIENVENIDO AL INICIO DE SESIÓN")
-        print("--------------------------------------")
-        IniciarSesion(df)
+        
+                
+        borrarVentana(window)
+        
+        titulosPantallas(window,mouse,'Inicia Sesión con Studify',screen_width,screen_height,'#DEB841')
+ 
+                
+        IniciarSesion(df,window,mouse,screen_width,screen_height)
         
         
         #PREGUNTA PARA REPETIR EL CODIGO
@@ -40,26 +88,59 @@ while ciclo == True:
 
     elif opcion == 2:
        
-        print("BIENVENIDO AL REGISTRO")
-        print("--------------------------------------")
+        borrarVentana(window)
  
-        Registrarse(df)
+        
+        titulosPantallas(window,mouse,'Registrate en Studify',screen_width,screen_height,'#DE9E36')
+ 
+ 
+ 
+        Registrarse(df,mouse,screen_width,screen_height)
  
         #PREGUNTA PARA REPETIR EL CODIGO
         ciclo = pregunta(ciclo)
         
     elif opcion == 3:
-        print("BIENVENIDO A LA MODIFICACIÓN DE DATOS")
-        print("--------------------------------------")
+        borrarVentana(window)
         
-        CambiarDatos(df)
+        titulosPantallas(window,mouse,'Modifica tus datos de Studify',screen_width,screen_height,'#BFBDC1')
+ 
+        
+        
+        CambiarDatos(df,mouse,screen_width,screen_height)
         
         #PREGUNTA PARA REPETIR EL CODIGO
         ciclo = pregunta(ciclo)
         
     elif opcion == 4:
+
+        borrarVentana(window)
         
-        print(df)
+        window.bgcolor("#DEB841")
+        
+        mouse.goto(-30,350)
+        
+        # Función para dibujar una celda de la tabla
+        def dibujar_celda(texto):
+            mouse.pendown()
+            mouse.forward(40)
+            mouse.write(texto, align="center", font=("Arial", 16))
+            mouse.forward(40)
+            mouse.penup()
+
+        # Dibujar las etiquetas de columna
+        etiquetas = df.columns.tolist()
+        for etiqueta in etiquetas:
+            dibujar_celda(etiqueta)
+            
+        mouse.goto(-30,330)
+        # Dibujar las filas de datos
+        for _, fila in df.iterrows():
+            valores = fila.values.tolist()
+            for valor in valores:
+                dibujar_celda(str(valor))
+
+            mouse.goto(-30, mouse.ycor() - 20)
         #PREGUNTA PARA REPETIR EL CODIGO
         ciclo = pregunta(ciclo)
         
@@ -68,7 +149,12 @@ while ciclo == True:
         print("Saliendo del programa")
         break
     
-
+    #elif opcion == 6:
+    #    GraficaRatingPlataforma()
+    #    GraficaCategoriaInteresados()
+    #    GraficaCategoriaSuscritos()
+    #    EstadisticasGlobales() 
+    
     elif opcion > 5 or opcion <= 0:
         print("Estas opciones no existen")
         
--- modulos.py
+++ modulos.py
@@ -3,9 +3,38 @@ import pandas as pd
 import os
 import sys
 import matplotlib.pyplot as plt
+import turtle
+
 
 dfDatos = pd.read_csv("Datos.csv")
 
+# Función para borrar el contenido de la ventana
+def borrarVentana(window):
+    window.clear()
+    
+    print("INGRESE SU OPCIÓN AQUÍ EN CONSOLA")
+
+def titulosPantallas(window,mouse,titulo,screen_width,screen_height,color):
+    
+    window.title("STUDIFY")
+    window.bgcolor(color)
+    texto_inicio = titulo
+    mouse.hideturtle()
+    font_size = 40
+    mouse.color("#37323E")
+
+    #Posicionamiento en el centro
+    pos_x = screen_width / 2
+    pos_y = screen_height / 2
+
+    mouse.penup()
+    mouse.goto(pos_x, pos_y)
+
+    font = ("Comic Sans MS", font_size, "bold")
+    mouse.write(texto_inicio, align="center", font=font)
+        
+ 
+    
 #FUNCIÓN PARA PODER REGRESAR AL MENU O SALIR
 def pregunta(estado):
     while True:
@@ -31,7 +60,7 @@ def pregunta(estado):
 
      
 #FUNCION PARA REGISTRAR UN NUEVO USUARIO
-def Registrarse(df): 
+def Registrarse(df,mouse,screen_width,screen_height): 
     #df = pd.DataFrame(columns=['Usuario', 'Contraseña'])  
     name = input("Ingrese el nombre de la cuenta: ")
     password = input("Ingrese la contraseña de la cuenta: ")
@@ -40,12 +69,16 @@ def Registrarse(df):
 
     dfNew = pd.concat([df, addrow])
     dfNew.to_csv("Usuarios.csv", index=False)
-    print(dfNew)
+    
+    mouse.goto((screen_width/2)-10,(screen_height/2)-30)
+    texto = "Buena elección: " + name + ", bienvenido!"
+    mouse.write(texto,align="center", font=("Comic Sans MS", 24, "bold"))
+    
 
     return dfNew
 
 #FUNCION PARA REGISTRARSE CON EL USUARIO YA CREADO
-def IniciarSesion(df):
+def IniciarSesion(df,window,mouse,screen_width,screen_height):
     logged_in = False  # Variable para controlar el inicio de sesión
 
     while not logged_in:  # Repetir hasta que se inicie sesión correctamente
@@ -61,22 +94,41 @@ def IniciarSesion(df):
             print("Inicio de sesión exitoso")
             logged_in = True  # Actualizar el estado de inicio de sesión
             
+            
+            mouse.goto((screen_width/2)-10,(screen_height/2)-30)
+            texto = "Bienvenido usuario: " + name
+            mouse.write(texto,align="center", font=("Comic Sans MS", 24, "bold"))
+            
             os.system("pause")
             os.system("cls")
+            
             #FUNCIÓN PARA ACCEDER A LAS CATEGORIAS
-            Categorias()
+            Categorias(window,mouse,screen_width,screen_height)
         
          
-def CambiarDatos(df):
+def CambiarDatos(df,mouse,screen_width,screen_height):
     # Solicitar nombre de usuario y contraseña
     name = input("Ingrese el usuario de su cuenta: ")
     password = input("Ingrese la contraseña de su cuenta: ")
+    
+    
+
+    
+    
+    
     
     # Verificar coincidencia exacta de nombre de usuario y contraseña en el DataFrame
     maskName = df['Usuario'] == name
     maskPassword = df['Password'] == password
     
     if maskName.any() and maskPassword.any():
+        
+            
+        mouse.goto((screen_width/2)-10,(screen_height/2)-30)
+        texto = "Ingresado como: " + name
+        mouse.write(texto,align="center", font=("Comic Sans MS", 24, "bold"))
+        
+        
         # Si hay coincidencia exacta, solicitar opción de modificación
         option = input("Seleccione una opción para modificación: \n1. Modificar nombre \n2. Modificar contraseña \n")
         
@@ -88,10 +140,22 @@ def CambiarDatos(df):
                 newName = input("Ingrese el nuevo nombre de su cuenta: ")
                 df.loc[maskName, 'Usuario'] = newName
                 
+                mouse.goto((screen_width/2)-10,(screen_height/2)-60)
+                texto = "Usuario cambiado a: " + newName
+                mouse.write(texto,align="center", font=("Comic Sans MS", 24, "bold"))
+    
+
+                
             elif option == 2:
                 # Modificar contraseña
                 newPassword = input("Ingrese la nueva contraseña de su cuenta: ")
                 df.loc[maskPassword, 'Password'] = newPassword
+                
+                
+                mouse.goto((screen_width/2)-10,(screen_height/2)-60)
+                texto = "Contraseña cambiada a: " + newPassword
+                mouse.write(texto,align="center", font=("Comic Sans MS", 24, "bold"))
+    
                 
             else:
                 # Opción no válida
@@ -102,6 +166,11 @@ def CambiarDatos(df):
             print("Opción no válida, ingrese de nuevo")
             
     else:
+        
+        mouse.goto((screen_width/2)-10,(screen_height/2)-30)
+        texto = "Error"
+        mouse.write(texto,align="center", font=("Comic Sans MS", 24, "bold"))
+        
         # No hay coincidencia exacta de nombre de usuario y contraseña
         print("Datos no proporcionados o agregados con anterioridad")
     
@@ -113,10 +182,51 @@ def CambiarDatos(df):
 
 
 
-def Categorias():
+def Categorias(window,mouse,screen_width,screen_height):
+    borrarVentana(window)
+    
+    window.title("CATEGORÍAS")
+    window.bgcolor("#DE9E36")
+    texto_inicio = "Selecciona su categoría a ver"
+    mouse.hideturtle()
+    font_size = 40
+    mouse.color("#37323E")
+
+    #Posicionamiento en el centro
+    pos_x = screen_width / 2
+    pos_y = screen_height / 1.5
+
+    mouse.penup()
+    mouse.goto(pos_x, pos_y)
+
+    font = ("Comic Sans MS", font_size, "bold")
+    mouse.write(texto_inicio, align="center", font=font)
+    
+            
+    # Lista de los títulos
+    lista_titulos = ["Arte", "Numérico", "Emprendimiento", "Idiomas", "Ciencias", "Tecnología"]
+
+    mouse.goto(60, 125)
+    
+    # Ciclo para mostrar los títulos en dos filas de tres columnas
+    for i in range(2):
+        for j in range(3):
+            # Obtener el índice del título correspondiente
+            index = i * 3 + j
+            # Mostrar el título en la posición actual
+            mouse.write(str(index+1)+") "+lista_titulos[index], align="center", font=("Arial", 24, "bold"))
+            mouse.forward(125)
+
+        # Mover a la siguiente línea
+        mouse.goto(60, mouse.ycor() - 50)
+
+
+
+
+
     while True:
         try:
-            opCat = int(input("Elige una categoría \n 1. Arte \n 2. Numérico \n 3. Negocios \n 4. Idiomas \n 5. Ciencias \n 6. Tecnología \n Presiona 7 para salir. \n"))
+            opCat = int(input("Ingrese el número de su opción según la interfaz gráfica \nSi desea salir presione 7\n"))
         except ValueError:
             print("Opciones no válidas")
             os.system("pause")
@@ -133,29 +243,29 @@ def Categorias():
         os.system("cls")
         #Menú cursos de arte
         if opCat == 1:
-            InfoCategoria("Arte")
+            InfoCategoria("Arte",mouse,window,screen_width,screen_height)
         #Menú cursos numéricos
         elif opCat == 2:
-            InfoCategoria("Numerico")
-        #Menú cursos de negocios
+            InfoCategoria("Numerico",mouse,window,screen_width,screen_height)
+        #Menú cursos de Emprendimiento
         elif opCat == 3:
-            InfoCategoria("Emprendimiento")
+            InfoCategoria("Emprendimiento",mouse,window,screen_width,screen_height)
         #Menú cursos de idiomas
         elif opCat == 4:
-            InfoCategoria("Idiomas")
+            InfoCategoria("Idiomas",mouse,window,screen_width,screen_height)
         #Menú cursos de ciencias    
         elif opCat == 5:
-            InfoCategoria("Ciencia")
+            InfoCategoria("Ciencia",mouse,window,screen_width,screen_height)
         #Menú cursos de tecnología
         elif opCat == 6:
-            InfoCategoria("Computacional")
+            InfoCategoria("Computacional",mouse,window,screen_width,screen_height)
         else:
             print("Saliendo de las categorías.... \n Regresando al menú principal")
 
         break
 
 
-def InfoCategoria(categoria):
+def InfoCategoria(categoria,mouse,window,screen_width,screen_height):
     dfCategoria = dfDatos[dfDatos["courseCategory"] == categoria]
     
     # Cálculo de interesados
@@ -165,20 +275,57 @@ def InfoCategoria(categoria):
     suscriptores = dfCategoria["suscribersCategory"].sum()
     
     # Cálculo de calificación promedio de la categoría
-    calificacionCategoria = dfCategoria["platformRating"].mean()
+    calificacionCategoria = dfCategoria["platformRating"].mean().round(2)
     
     dfCursos = dfCategoria["course"].values
     
-    print(f"{categoria}")
-    print(f"{interesados} personas se han interesado en esta categoría.")
-    print(f"{suscriptores} personas se han suscrito a este curso desde Studify.")
-    print(f"Esta categoría tiene una clasificación de {calificacionCategoria} por parte de los suscriptores de Studify.")
-    print("Estos son los cursos más populares de esta categoría: ")
     
+    borrarVentana(window)
+    
+    window.title(categoria)
+    window.bgcolor("#2b2e74")
+    texto_inicio = categoria
+    mouse.hideturtle()
+    font_size = 40
+    mouse.color("#FFF")
+
+    #Posicionamiento en el centro
+    pos_x = screen_width / 2
+    pos_y = screen_height / 1.3
+
+    mouse.penup()
+    mouse.goto(pos_x, pos_y)
+
+    font = ("Comic Sans MS", font_size, "bold")
+    mouse.write(texto_inicio, align="center", font=font)
+    
+    
+    interesadosText = str(interesados) + " personas se han interesado en esta categoría."
+    suscriptoresText = str(suscriptores) + " personas se han suscrito a este curso desde Studify."
+    calificacionText = "Esta categoría tiene una clasificación de " + str(calificacionCategoria) + " por parte de los suscriptores de Studify."
+    cursosText = "Estos son los cursos más populares de esta categoría: "
+    
+    textosEscribir = [interesadosText,suscriptoresText,calificacionText,cursosText]
+    mouse.goto(200,200)
+    for i in range(len(textosEscribir)):
+        mouse.goto(200,200-(i*20))
+        mouse.write(textosEscribir[i], align="center", font=("Arial", 16))
+        
+    mouse.goto(200,120)
+    mouse.write("-"*150, align="center", font=("Arial", 16))
+    cursos = []
     i = 1
     for curso in dfCursos:   
-        print(f"{i}. {curso}")
+        cursos.append(curso)
         i += 1
+        
+        
+    mouse.goto(200,100)
+    for i in range(len(cursos)):
+        mouse.goto(200,100-(i*20))
+        mouse.write(str(i+1)+") "+cursos[i], align="center", font=("Arial", 16))
+    
+
     
     while True:
         # Pregunta al usuario si desea ver la información de alguno de los cursos
@@ -196,7 +343,7 @@ def InfoCategoria(categoria):
                     
                     os.system("pause")
                     os.system("cls")
-                    InfoCursos(opCur - 1, dfCategoria)
+                    InfoCursos(opCur - 1, dfCategoria,mouse,window,screen_width,screen_height)
                     
                 except ValueError:
                     print("Opción no válida")
@@ -212,16 +359,46 @@ def InfoCategoria(categoria):
             print("Opción no válida")
 
 
-def InfoCursos(index, dfCategoria):
+def InfoCursos(index, dfCategoria,mouse,window,screen_width,screen_height):
 
     calificacion= dfCategoria["userRating"].values[index]
     descripcion = dfCategoria["Descripcion"].values[index]
     inscrito = dfCategoria["userEnrolled"].values[index]
     curso = dfCategoria["course"].values[index]
-    print(f"{curso}")
-    print(f"Este curso tiene una calificación de {calificacion} por los suscriptores de Studify.")
-    print(f"{descripcion}")
-    print(f"{inscrito} personas se han inscrito a este curso desde Studify.")
+    
+    
+    borrarVentana(window)
+    
+    window.title(curso)
+    window.bgcolor("#05604E")
+    texto_inicio = curso
+    mouse.hideturtle()
+    font_size = 20
+    mouse.color("#FFF")
+
+    #Posicionamiento en el centro
+    pos_x = screen_width / 2
+    pos_y = screen_height / 1.3
+
+    mouse.penup()
+    mouse.goto(pos_x, pos_y)
+
+    font = ("Comic Sans MS", font_size, "bold")
+    mouse.write(texto_inicio, align="center", font=font)
+    
+    calificacionText = "Este curso tiene una calificación de "+ str(calificacion)+" por los suscriptores de Studify."
+    descripcionText = descripcion
+    inscritosText = str(inscrito) + " personas se han inscrito a este curso desde Studify."
+    
+    textosEscribir = [calificacionText,inscritosText,descripcionText]
+    mouse.goto(200,200)
+    for i in range(len(textosEscribir)):
+        mouse.goto(200,200-(i*20))
+        
+        
+        mouse.write(textosEscribir[i], align="center", font=("Arial", 16))
+        
+    
 
 def GraficaRatingPlataforma():
     dfInfo = dfDatos[["platform", "platformRating"]]
